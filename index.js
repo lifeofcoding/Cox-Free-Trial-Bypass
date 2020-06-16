a1b93a45bfbd295fb3436cbe7efca997ace06027
LifeOfCoding <LifeOfCoding@gmail.com>
2020-06-16 04:32:48 -0400

    Removed shell file added icons to notifications

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ const argv = require("yargs")
     description: "Time to wait for page loads",
   }).argv;
 
+const cmd = `
+  macchanger -s "${argv.iface}" && \
+  sudo service network-manager stop && \
+  sudo ifconfig "${argv.iface}" down && \
+  sudo macchanger -a "${argv.iface}" && \
+  macchanger -s "${argv.iface}" && \
+  sudo ifconfig "${argv.iface}" up && \
+  sudo service network-manager start
+`;
+
 const rand = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -68,195 +78,189 @@ const emailMixer = (firstName, lastName) => {
     ignoreHTTPSErrors: true,
   };
 
-  sh.exec(
-    `bash ./Macchangerizer.sh ${argv.iface} && sleep 10`,
-    async (code, output) => {
-      notifier.notify({
-        title: "Cox Wifi Connecting",
-        message: "Attempting to connect to Cox Wifi Trial Wifi...",
+  sh.exec(cmd, async (code, output) => {
+    notifier.notify({
+      icon: path.join(__dirname, "wifi.png"),
+      title: "Cox Wifi Connecting...",
+      message: "Attempting to connect to Cox Wifi.",
+    });
+
+    var macParts = output.match(/(?<=New MAC:       \s*).*?(?=\s* )/gs);
+
+    const mac = macParts[0];
+
+    await new Promise((r) => setTimeout(r, argv.timeout));
+
+    const browser = await puppeteer.launch(options);
+
+    const context = await browser.createIncognitoBrowserContext();
+
+    const page = await context.newPage();
+
+    try {
+      page.on("error", (msg) => {
+        throw msg;
       });
 
-      var macParts = output.match(/(?<=New MAC:       \s*).*?(?=\s* )/gs);
+      const preloadFile = fs.readFileSync("./preload.js", "utf8");
+      await page.evaluateOnNewDocument(preloadFile);
 
-      const mac = macParts[0];
+      await page.goto(
+        `http://cwifi-new.cox.com/?mac-address=${mac}&ap-mac=${randomMac()}&ssid=CoxWiFi&vlan=103&nas-id=BTNRWAGB01.at.at.cox.net&block=false&unique=$HASH`,
+        {
+          waitUntil: "networkidle2",
+          timeout: 60000,
+        }
+      );
 
-      await new Promise((r) => setTimeout(r, argv.timeout));
-
-      const browser = await puppeteer.launch(options);
-
-      const context = await browser.createIncognitoBrowserContext();
-
-      const page = await context.newPage();
-
-      try {
-        page.on("error", (msg) => {
-          throw msg;
+      if (argv.debug) {
+        await page.screenshot({
+          path: path.resolve(__dirname) + "/landing.jpeg",
+          type: "jpeg",
+          quality: 100,
         });
 
-        const preloadFile = fs.readFileSync("./preload.js", "utf8");
-        await page.evaluateOnNewDocument(preloadFile);
-
-        await page.goto(
-          `http://cwifi-new.cox.com/?mac-address=${mac}&ap-mac=${randomMac()}&ssid=CoxWiFi&vlan=103&nas-id=BTNRWAGB01.at.at.cox.net&block=false&unique=$HASH`,
-          {
-            waitUntil: "networkidle2",
-            timeout: 60000,
-          }
+        console.log(
+          "[DEBUG]: Landing page screenshot: ",
+          path.resolve(__dirname) + "/landing.jpeg"
         );
-
-        if (argv.debug) {
-          await page.screenshot({
-            path: path.resolve(__dirname) + "/landing.jpeg",
-            type: "jpeg",
-            quality: 100,
-          });
-
-          console.log(
-            "[DEBUG]: Landing page screenshot: ",
-            path.resolve(__dirname) + "/landing.jpeg"
-          );
-        }
-
-        await page.waitForSelector(
-          "#signIn > .signInText > .freeAccessPassSignup > .floatleft > .coxRegisterButton"
-        );
-        await page.keyboard.down("Tab");
-        await page.keyboard.down("Tab");
-        await page.keyboard.press("Enter");
-
-        await page.waitForNavigation({ timeout: argv.timeout });
-
-        var userAgent = await page.evaluate(() => {
-          return (function () {
-            return window.navigator.userAgent;
-          })();
-        });
-
-        if (argv.debug) {
-          console.log("Using usere-agent:", userAgent);
-        }
-
-        await page.setViewport({ width: 1440, height: 779 });
-
-        await page.waitForSelector(
-          "table #trial_request_voucher_form_firstName"
-        );
-        await page.click("table #trial_request_voucher_form_firstName");
-
-        await page.type(
-          "table #trial_request_voucher_form_firstName",
-          firstName,
-          {
-            delay: rand(100, 300),
-          }
-        );
-
-        await page.type(
-          "table #trial_request_voucher_form_lastName",
-          lastName,
-          {
-            delay: rand(100, 300),
-          }
-        );
-
-        await page.waitForSelector("table #trial_request_voucher_form_isp");
-        await page.click("table #trial_request_voucher_form_isp");
-
-        await page.select("table #trial_request_voucher_form_isp", "Verizon");
-
-        await page.waitForSelector("table #trial_request_voucher_form_email");
-        await page.click("table #trial_request_voucher_form_email");
-
-        await page.type(
-          "table #trial_request_voucher_form_email",
-          emailMixer(firstName, lastName),
-          {
-            delay: rand(100, 300),
-          }
-        );
-
-        await page.waitForSelector(
-          ".decisionBlock > table > tbody > tr > .top:nth-child(2)"
-        );
-        await page.click(
-          ".decisionBlock > table > tbody > tr > .top:nth-child(2)"
-        );
-
-        await page.waitForSelector(
-          "table #trial_request_voucher_form_serviceTerms"
-        );
-        await page.click("table #trial_request_voucher_form_serviceTerms");
-
-        await page.keyboard.down("Tab");
-        await page.keyboard.down("Tab");
-        await page.keyboard.press("Enter");
-
-        await page.waitForNavigation({ timeout: argv.timeout });
-
-        var pageText = await page.evaluate(() => {
-          return (function () {
-            var s = window.getSelection();
-            s.removeAllRanges();
-            var r = document.createRange();
-            r.selectNode(document.body);
-            s.addRange(r);
-            var c = s.toString();
-            s.removeAllRanges();
-            return c;
-          })();
-        });
-
-        if (argv.debug) {
-          console.log("[DEBUG]: pageText:", pageText);
-        }
-
-        if (pageText.toLowerCase().includes("you are now connected")) {
-          let t = new Date().toLocaleString();
-
-          console.log("Wifi Connected Successfully", t);
-
-          notifier.notify({
-            title: "Cox Wifi Connected",
-            message: "Wifi Connected Successfully",
-          });
-
-          if (argv.debug) {
-            await page.screenshot({
-              path: path.resolve(__dirname) + "/result.jpeg",
-              type: "jpeg",
-              quality: 100,
-            });
-
-            console.log(
-              "[DEBUG]: Result page screenshot: ",
-              path.resolve(__dirname) + "/result.jpeg"
-            );
-          }
-
-          setTimeout(run, 60000 * 60);
-        } else {
-          await page.screenshot({
-            path: path.resolve(__dirname) + "/error-result.jpeg",
-            type: "jpeg",
-            quality: 100,
-          });
-
-          console.log(
-            "[DEBUG]: Error screenshot: ",
-            path.resolve(__dirname) + "/error-result.jpeg"
-          );
-
-          notifier.notify({
-            title: "Error",
-            message: "Error, Cox Wifi failed to connect, please check output.",
-          });
-        }
-
-        await browser.close();
-      } catch (err) {
-        console.warn("Error: ", err);
-        setTimeout(run, 30000);
       }
+
+      await page.waitForSelector(
+        "#signIn > .signInText > .freeAccessPassSignup > .floatleft > .coxRegisterButton"
+      );
+      await page.keyboard.down("Tab");
+      await page.keyboard.down("Tab");
+      await page.keyboard.press("Enter");
+
+      await page.waitForNavigation({ timeout: argv.timeout });
+
+      var userAgent = await page.evaluate(() => {
+        return (function () {
+          return window.navigator.userAgent;
+        })();
+      });
+
+      if (argv.debug) {
+        console.log("Using usere-agent:", userAgent);
+      }
+
+      await page.setViewport({ width: 1440, height: 779 });
+
+      await page.waitForSelector("table #trial_request_voucher_form_firstName");
+      await page.click("table #trial_request_voucher_form_firstName");
+
+      await page.type(
+        "table #trial_request_voucher_form_firstName",
+        firstName,
+        {
+          delay: rand(100, 300),
+        }
+      );
+
+      await page.type("table #trial_request_voucher_form_lastName", lastName, {
+        delay: rand(100, 300),
+      });
+
+      await page.waitForSelector("table #trial_request_voucher_form_isp");
+      await page.click("table #trial_request_voucher_form_isp");
+
+      await page.select("table #trial_request_voucher_form_isp", "Verizon");
+
+      await page.waitForSelector("table #trial_request_voucher_form_email");
+      await page.click("table #trial_request_voucher_form_email");
+
+      await page.type(
+        "table #trial_request_voucher_form_email",
+        emailMixer(firstName, lastName),
+        {
+          delay: rand(100, 300),
+        }
+      );
+
+      await page.waitForSelector(
+        ".decisionBlock > table > tbody > tr > .top:nth-child(2)"
+      );
+      await page.click(
+        ".decisionBlock > table > tbody > tr > .top:nth-child(2)"
+      );
+
+      await page.waitForSelector(
+        "table #trial_request_voucher_form_serviceTerms"
+      );
+      await page.click("table #trial_request_voucher_form_serviceTerms");
+
+      await page.keyboard.down("Tab");
+      await page.keyboard.down("Tab");
+      await page.keyboard.press("Enter");
+
+      await page.waitForNavigation({ timeout: argv.timeout });
+
+      var pageText = await page.evaluate(() => {
+        return (function () {
+          var s = window.getSelection();
+          s.removeAllRanges();
+          var r = document.createRange();
+          r.selectNode(document.body);
+          s.addRange(r);
+          var c = s.toString();
+          s.removeAllRanges();
+          return c;
+        })();
+      });
+
+      if (argv.debug) {
+        console.log("[DEBUG]: pageText:", pageText);
+      }
+
+      if (pageText.toLowerCase().includes("you are now connected")) {
+        let t = new Date().toLocaleString();
+
+        notifier.notify({
+          icon: path.join(__dirname, "wifi.png"),
+          title: "Cox Wifi Connected",
+          message: "Wifi Connected Successfully",
+        });
+
+        console.log("Wifi Connected Successfully", t);
+
+        if (argv.debug) {
+          await page.screenshot({
+            path: path.resolve(__dirname) + "/result.jpeg",
+            type: "jpeg",
+            quality: 100,
+          });
+
+          console.log(
+            "[DEBUG]: Result page screenshot: ",
+            path.resolve(__dirname) + "/result.jpeg"
+          );
+        }
+
+        setTimeout(run, 60000 * 60);
+      } else {
+        await page.screenshot({
+          path: path.resolve(__dirname) + "/error-result.jpeg",
+          type: "jpeg",
+          quality: 100,
+        });
+
+        console.log(
+          "[DEBUG]: Error screenshot: ",
+          path.resolve(__dirname) + "/error-result.jpeg"
+        );
+
+        notifier.notify({
+          icon: path.join(__dirname, "error.png"),
+          title: "Error",
+          message: "Error, Cox Wifi failed to connect, please check output.",
+        });
+      }
+
+      await browser.close();
+    } catch (err) {
+      console.warn("Error: ", err);
+      setTimeout(run, 30000);
     }
-  );
+  });
 })();
